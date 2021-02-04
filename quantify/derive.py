b9eb5be014ec17f3298cec6ca3bb5befd2fb6ddf
vandrw <v.andrei1455@gmail.com>
2021-02-04 23:44:12 +0100

    final changes

--- quantify/derive.py
+++ quantify/derive.py
@@ -4,166 +4,187 @@ import pickle
 import math
 import matplotlib.pyplot as plt
 
+"""
+Self Notes:
+    For every ngram, we calculate I given every chord
+    P(e_i| e_i-(n-1) ... ) = P(e_i-(n-1) ... and e_i) / P(e_i)
+
+    P(e) = freq of chord / nr of chords
+    P(e_i-(n-1) ... and e) = freq of ngram / nr of ngrams (of the same order)
+
+    H will be calculated by reducing the size of the ngram,
+    starting from 1 before the chord to the first one
+    e.g. Eb:maj F:min9 Bb:sus4(b7) Eb:maj Ab:min9 Db:9
+    First  : Ab:min9 Db:9
+    Second : Eb:maj Ab:min9 Db:9
+    Third  : Bb:sus4(b7) Eb:maj Ab:min9 Db:9
+    ...
+
+    Each will be summed.
+
+    entropy is uncertainity
+    infor_content is surprise
+"""
+
 
 class PPM:
+    """
+    Prediction by Partial Matchin algorithm.
+    """
     def __init__(self):
-        # chord freq/probs
-        # ngram freq/entropy
-        pass
+        self.chord_freq = {}
+        self.ngram_freq = {}
+        self.content = {}
+        self.entropy = {}
 
-    def countChords(self, song_sequence):
-        pass
+    def countChord(self, chord):
+        if chord not in self.chord_freq:
+            self.chord_freq[chord] = 1
+        else:
+            self.chord_freq[chord] += 1
 
-    def countNgram(self, ngram):
-        pass
+    def countNgram(self, chord, ngram):
+        if chord not in self.ngram_freq:
+            self.ngram_freq[chord] = {}
+            self.ngram_freq[chord][ngram] = 1
+        else:
+            if ngram not in self.ngram_freq[chord]:
+                self.ngram_freq[chord][ngram] = 1
+            else:
+                self.ngram_freq[chord][ngram] += 1
+
+    def probChords(self):
+        # Calculating the probability of each chord.
+        nr_chords = len(self.chord_freq.keys())
+        for chord in self.chord_freq.keys():
+            self.chord_freq[chord] /= nr_chords
+
+    def countNgramLen(self):
+        lengths = {}
+        for chord in self.ngram_freq.keys():
+            for seq in self.ngram_freq[chord]:
+                if len(seq) in lengths:
+                    lengths[len(seq)] += 1
+                else:
+                    lengths[len(seq)] = 1
+        
+        return lengths
+
+    def calcInfoContent(self, lengths):
+        self.content = copy.deepcopy(self.ngram_freq)
+        for chord in self.ngram_freq.keys():
+            for seq in self.ngram_freq[chord]:
+                self.ngram_freq[chord][seq] = (
+                    self.ngram_freq[chord][seq] / lengths[len(seq)]) / \
+                    self.chord_freq[chord]
+
+                self.content[chord][seq] = (-1) * \
+                    math.log2(self.ngram_freq[chord][seq])
+
+    def calcEntropy(self):
+        self.entropy = copy.deepcopy(self.ngram_freq)
+        for chord in self.entropy.keys():
+            for seq in self.entropy[chord]:
+                self.entropy[chord][seq] = 0
+
+        for chord in self.ngram_freq.keys():
+            for seq in self.ngram_freq[chord]:
+                self.entropy[chord][seq] += self.ngram_freq[chord][seq] * \
+                    self.content[chord][seq]
 
     def saveData(self):
-        pass
+        with open("quantify/objects/ngram_data.pkl", "wb") as ngram_output:
+            pickle.dump(self.ngram_freq, ngram_output, pickle.HIGHEST_PROTOCOL)
+        with open("quantify/objects/chord_data.pkl", "wb") as chord_output:
+            pickle.dump(self.chord_freq, chord_output, pickle.HIGHEST_PROTOCOL)
 
     def loadData(self):
-        pass
+        with open("quantify/ngram_data.pkl", "rb") as ngram_output:
+            self.ngram_freq = pickle.load(ngram_output)
+        with open("quantify/chord_data.pkl", "rb") as chord_output:
+            self.chord_freq = pickle.load(chord_output)
 
+    def plotMetrics(self, uncertainity, surprise):
+        plt.scatter(uncertainity, surprise)
+        plt.xlabel("Uncertainty (bits)")
+        plt.ylabel("Surprise (bits)")
+        plt.show()
 
-def PPM(file_path: str):
-    """
+    def train(self, file_path):
+        """
+        Train on a Corpus of song sequences, given by the file_path.
+        """
+        with open(file_path, 'r') as chords:
+            for line in chords:
+                line_chords = line.split(' ')
+                song_length = len(line_chords)
 
-    """
-    # chord_dict = {}
-    # ngrams = {}
-    # with open(file_path, 'r') as chords:
-    #     for line in chords:
-    #         line_chords = line.split(' ')
-    #         song_length = len(line_chords)
+                # Calculating the occurence of a chord in
+                # the entire dataset
+                for i in range(0, song_length):
+                    curr_chord = line_chords[i]
+                    self.countChord(curr_chord)
 
-    #         # Calculating the occurence of a chord in
-    #         # the entire dataset
-    #         for i in range(0, song_length):
-    #             curr_chord = line_chords[i]
-    #             if curr_chord not in chord_dict:
-    #                 chord_dict[curr_chord] = 1
-    #             else:
-    #                 chord_dict[curr_chord] += 1
+                    # Not sure here... Calculating the occurence
+                    # of engrams that contain the previous chord?
+                    if i > 0:
+                        for j in range(i, 0, -1):
+                            ngram = ' '.join(line_chords[j-1:i])
+                            self.countNgram(curr_chord, ngram)
+                    else:
+                        self.countNgram(curr_chord,
+                                        "start " + curr_chord)
 
-    #             # Not sure here... Calculating the occurence
-    #             # of engrams that contain the previous chord?
-    #             if i > 0:
-    #                 for j in range(i, 0, -1):
-    #                     ngram = ' '.join(line_chords[j-1:i])
-    #                     # print(ngram)
-    #                     if curr_chord not in ngrams:
-    #                         ngrams[curr_chord] = {}
-    #                         ngrams[curr_chord][ngram] = 1
-    #                     else:
-    #                         if ngram not in ngrams[curr_chord]:
-    #                             ngrams[curr_chord][ngram] = 1
-    #                         else:
-    #                             ngrams[curr_chord][ngram] += 1
-    #             else:
-    #                 if curr_chord not in ngrams:
-    #                     ngrams[curr_chord] = {}
-    #                     ngrams[curr_chord]["start " + curr_chord] = 1
-    #                 else:
-    #                     if "start " + curr_chord not in ngrams[curr_chord]:
-    #                         ngrams[curr_chord]["start " + curr_chord] = 1
-    #                     else:
-    #                         ngrams[curr_chord]["start " + curr_chord] += 1
+        # Count of each sequence size (to calculate information 
+        # content based on order)
+        lengths = self.countNgramLen()
 
-    # print("Finished parsing songs!")
-    # with open("quantify/ngram_data.pkl", "wb") as ngram_output:
-    #     pickle.dump(ngrams, ngram_output, pickle.HIGHEST_PROTOCOL)
-    # with open("quantify/chord_data.pkl", "wb") as chord_output:
-    #     pickle.dump(chord_dict, chord_output, pickle.HIGHEST_PROTOCOL)
+        # Calculate information content
+        self.calcInfoContent(lengths)
 
-    with open("quantify/ngram_data.pkl", "rb") as ngram_output:
-        ngrams = pickle.load(ngram_output)
-    with open("quantify/chord_data.pkl", "rb") as chord_output:
-        chord_dict = pickle.load(chord_output)
+        # Entropy of each progression(sequence) + prob of each 
+        # sequence, given a chord.
+        self.calcEntropy()
 
-    # For every ngram, we calculate I given every chord
-    # P(e_i| e_i-(n-1) ... ) = P(e_i-(n-1) ... and e_i) / P(e_i)
-    nr_ngrams = sum(len(ngrams[n]) for n in ngrams.keys())
-    nr_chords = len(chord_dict.keys())
+    def predict(self, file_path):
+        uncer = []
+        surpr = []
+        with open(file_path, 'r') as chords:
+            for line in chords:
+                line_chords = line.split(' ')
+                song_length = len(line_chords)
 
-    # Probability of each chord.
-    for chord in chord_dict.keys():
-        chord_dict[chord] /= nr_chords
+                # Calculating the occurence of a chord in
+                # the entire dataset
+                for i in range(0, song_length):
+                    curr_chord = line_chords[i]
 
-    # Entropy of each progression(sequence) + prob of each sequence, given a chord.
-    infor_content = copy.deepcopy(ngrams)
-    for chord in ngrams.keys():
-        for seq in ngrams[chord]:
-            ## TODO: change nr of ngrams to nr of ngrams with similar size
-            ngrams[chord][seq] = (ngrams[chord][seq] / nr_ngrams) / chord_dict[chord]
-            infor_content[chord][seq] = -math.log2(ngrams[chord][seq])
+                    if i > 0:
+                        ngram = ' '.join(line_chords[0:i])
+                        # Impossible to happen, since I check on the same set.
+                        # TODO: Change what's commented below later. Introduce
+                        # a very small proability in this case.
+                        # if ngram not in self.ngram_freq[curr_chord]:
 
-    print("Finished calculating entropy.")
-    # P(e) = freq of chord / nr of chords
-    # P(e_i-(n-1) ... and e) = freq of engram / nr of engrams
-
-    entropy = copy.deepcopy(ngrams)
-    for chord in entropy.keys():
-        for seq in entropy[chord]:
-            entropy[chord][seq] = 0
-
-    for chord in ngrams.keys():
-        for seq in ngrams[chord]:
-            entropy[chord][seq] += ngrams[chord][seq] * infor_content[chord][seq]
-
-    # entropy is uncertainity
-    # infor_content is surprise
-    uncer = []
-    surpr = []
-    with open(file_path, 'r') as chords:
-        for line in chords:
-            line_chords = line.split(' ')
-            song_length = len(line_chords)
-
-            # Calculating the occurence of a chord in
-            # the entire dataset
-            for i in range(0, song_length):
-                curr_chord = line_chords[i]
-                if curr_chord not in chord_dict:
-                    surpr.append(15)
-                    uncer.append(8)
-                    continue
-                
-
-                if i > 0:
-                    ngram = ' '.join(line_chords[0:i])
-                    # print(ngram)
-
-                    # Impossible to happen, since I check on the same set. 
-                    # TODO: Change this later (as well as the one before).
-                    if ngram not in ngrams[curr_chord]:
-                        surpr.append(15)
-                        uncer.append(8)
-                        break
-                    surpr.append(infor_content[curr_chord][ngram])
-                    uncer.append(entropy[curr_chord][ngram])
-                else:
-                    surpr.append(infor_content[curr_chord]["start " + curr_chord])
-                    uncer.append(entropy[curr_chord]["start " + curr_chord])
-                
-                break
+                        surpr.append(self.content[curr_chord][ngram])
+                        uncer.append(self.entropy[curr_chord][ngram])
+                    else:
+                        surpr.append(
+                            self.content[curr_chord]["start " + curr_chord])
+                        uncer.append(self.entropy[curr_chord]
+                                     ["start " + curr_chord])
         
-    plt.scatter(uncer, surpr)
-    plt.xlabel("Uncertainty (bits)")
-    plt.ylabel("Surprise (bits)")
-                    
-    # H will be calculated by reducing the size of the engram,
-    # starting from 1 before the chord to the first one
-    # e.g. Eb:maj F:min9 Bb:sus4(b7) Eb:maj Ab:min9 Db:9
-    # First  : Ab:min9 Db:9
-    # Second : Eb:maj Ab:min9 Db:9
-    # Third  : Bb:sus4(b7) Eb:maj Ab:min9 Db:9
-    # ...
-
-    # Each will be summed.
+        return uncer, surpr
 
 
 def main():
     start = time.time()
-    PPM("quantify/chords.txt")
+
+    ppm = PPM()
+    ppm.train("quantify/chords.txt")
+    uncer, surpr = ppm.predict("quantify/chords.txt")
+    ppm.plotMetrics(uncer, surpr)
+
     print("Finished in {}".format(time.time() - start))
 
 
